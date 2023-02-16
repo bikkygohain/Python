e67598c66a23b38ce1221bc9fac7c0becbe71bf6
Bikky Gohain <93116693+bikkygohain@users.noreply.github.com>
2023-02-16 20:41:27 +0530

    Create comments.py

---
+++ comments.py
@@ -0,0 +1,13 @@
+# Comments
+#   -   DON'T overuse comments!
+#   -   Code should be self-explanatory by using descriptive variable and function names
+
+#List items are indexed
+"""my_list = ["January", "February", "March"]
+print(my_list[2])
+my_list.append("April")
+print(my_list)
+print(my_list[3])"""
+# print(my_list[4])
+# Index greater than the no of elements present in the list
+# Throws "IndexError: list index out of range"
